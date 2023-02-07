2949da2cacbc8ea72dea6f58c2aa37f682b2df5b
d1av <davi4alves@gmail.com>
2023-02-07 00:35:58 -0300

    feat: taxes calculator

--- src/main/java/com/dio/ImpostoCalc.java
+++ src/main/java/com/dio/ImpostoCalc.java
@@ -6,13 +6,12 @@ import java.util.Scanner;
 public class ImpostoCalc {
     public static void main(String[] args) throws IOException {
         Scanner leitor = new Scanner(System.in);
-        //double renda = leitor.nextDouble();
-        double renda = 4520.00;
+        double renda = leitor.nextDouble();
         double imposto;
 
         if (renda > 2000) {
             imposto = calcularImposto(renda);
-            System.out.println("R$ " + imposto);
+            System.out.printf("R$ %.2f",imposto);
         } else {
             System.out.println("Isento");
         }
@@ -21,17 +20,31 @@ public class ImpostoCalc {
     }
 
     private static double calcularImposto(double renda) {
-        double impostoCobrados = 0;
-        if (renda < 3000) {
-            impostoCobrados += (renda - 2000) * 0.08;
+        double impostoCobrados = 0.00f;
+        if (renda > 2000) {
+            impostoCobrados = eigthPercent(renda);
         }
-        if (renda < 4500) {
-            impostoCobrados += (renda - 3000) * 0.18;
+        if (renda > 3000) {
+            impostoCobrados += eighteenPercent(renda);
         }
         if (renda > 4500) {
-            impostoCobrados += (renda - 4500) * 0.28;
+            impostoCobrados += twentyEigth(renda);
         }
         return impostoCobrados;
+    }
+
+    private static double twentyEigth(double renda) {
+        return (renda - 4500) * 0.28;
+    }
+
+    private static double eighteenPercent(double renda) {
+        double currentRenda = renda > 4500 ? 4500 : renda;
+        return (currentRenda - 3000) * 0.18;
+    }
+
+    private static double eigthPercent(double renda) {
+        double currentRenda = renda > 3000 ? 3000 : renda;
+        return (currentRenda - 2000) * 0.08;
     }
 
 }
